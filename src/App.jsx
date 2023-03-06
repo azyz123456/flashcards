14fffe34d6a6a3f181c0c6b7fe2e89be1bc9c1ee
Angelina Zheng <64802771+azyz123456@users.noreply.github.com>
2023-03-05 23:36:58 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import './App.css';
 import Card from './Card.jsx';
 import DATA from "./data.js";
+import Quiz from './Quiz.jsx';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 //keep everything in terms of objects until return statement
 
 function App() {
   const data = JSON.parse(DATA);
 
-  //initialie cards by mapping data to cards
+  //initialize cards by mapping data to cards
   let initialCards = [];
+  let initialOrder = [];
+
   for (let i=0; i<data.length; i++) {
     let card = {
       "flipped": false,
@@ -20,11 +30,33 @@ function App() {
       "img": data[i].img
     };
     initialCards.push(card);
+
+    initialOrder.push(i);
   }
 
   const [cards, setCards] = useState(initialCards);
-  const [card, setCard] = useState(cards[0]);
-  const [prevCard, setPrevCard] = useState(cards[0]);
+  const [index, setIndex] = useState(0);
+  const [order, setOrder] = useState(initialOrder);
+  console.log(order);
+
+  function shuffleCards() {
+    let arr = [...order];
+    let newOrder = [];
+    while (arr.length > 0) {
+      const index = Math.floor(Math.random() * arr.length);
+      newOrder.push(arr[index]);
+      arr = arr.filter(function (num) {
+        return num !== arr[index];
+    });
+    }
+    setOrder(newOrder);
+    setIndex(0);
+    console.log("new order: ", newOrder);
+  }
+
+  function test() {
+    console.log("clicked shuffle button");
+  }
     
   function flipCard(cardNumber) {
     //change the card flipped to the opposite value
@@ -41,39 +73,63 @@ function App() {
   }
 
   function clickNext(e) {
-    //draw a random next card when the right arrow is clicked
-    //ensures that the next card is not the same as the current card
-    let index = Math.floor(Math.random() * data.length);
-    while (index === card.number) {
-      index = Math.floor(Math.random() * data.length);
+    if (index === order.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index+1);
     }
-    setPrevCard(card);
-    setCard(cards[index]);
     e.stopPropagation();
   }
 
   function clickPrev(e) {
-    setCard(prevCard);
+    if (index === 0) {
+      setIndex(order.length - 1);
+    } else {
+      setIndex(index-1);
+    }
     e.stopPropagation();
   }
 
-  return (
-    <div className="App">
+  const app = (
+    <div className="main">
       <h1>React.js Flashcards</h1>
       <p>How much do you know about React? Test your frontend development knowledge here!</p>
-      <h4>{"Number of Cards: " + cards.length}</h4>
-      <Card 
-          flipped={card.flipped}
-          name={card.name}
-          answer={card.answer}
-          cardNumber={card.number}
-          difficulty={card.difficulty}
-          img={card.img}
-          onClick={flipCard}
-          onClickNext={clickNext}
-          onClickPrev={clickPrev}
-        />
-    </div>
+      <h4>{(index+1) + "/" + data.length}</h4>
+        <Card 
+            flipped={cards[order[index]].flipped}
+            name={cards[order[index]].name}
+            answer={cards[order[index]].answer}
+            cardNumber={cards[order[index]].number}
+            difficulty={cards[order[index]].difficulty}
+            img={cards[order[index]].img}
+            onClick={flipCard}
+            onClickNext={clickNext}
+            onClickPrev={clickPrev}
+          />
+          <div className="shuffle" onClick={shuffleCards}>
+            Shuffle
+          </div>
+      </div>
+  );
+
+  return (
+    <Router>
+      <div className="App">
+            <ul id="navbar">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/quiz">Quiz</Link>
+              </li>
+            </ul>
+          <Routes>
+            <Route path="/quiz" element={<Quiz cards={cards} />}> </Route>
+            <Route path="/" element={app}> </Route>
+          </Routes>
+      </div>
+    </Router>
+
   )
 }
 
---
+++ src/Quiz.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+
+function Quiz(props) {
+    const questions = [];
+    const initialGuesses = [];
+
+    for (let i=0; i<props.cards.length; i++) {
+        let guess = {
+            "answer": props.cards[i].answer,
+            "guess": ""
+        };
+        initialGuesses.push(guess);
+    }
+
+    const [guesses, setGuesses] = useState(initialGuesses);
+
+    for (let i=0; i<props.cards.length; i++) {
+        //create questions and input fields
+        let item = (
+            <div>
+                <p>{"Term: " + props.cards[i].name}</p>
+                <input
+                    type="text"
+                    placeholder="Definition"
+                    value={guesses[i].guess}
+                    onChange={(e) => handleChange(e, i)}
+                />
+            </div>
+        );
+        questions.push(item);
+    }
+
+    const [submitted, setSubmitted] = useState(false);
+    const [numCorrect, setNumCorrect] = useState(0);
+    const [message, setMessage] = useState("");
+
+    function handleChange(e, index) {
+        let newGuesses = [...guesses];
+        for (let i=0; i<guesses.length; i++) {
+            if (index === i) {
+                newGuesses[i].guess = e.target.value;
+            }
+        }
+        setGuesses(newGuesses);
+    }
+
+    function handleSubmit() {
+        //let user check if their answer is correct
+
+        for (let i=0; i<guesses.length; i++) {
+            if (guesses[i].guess === "") {
+                setMessage("Please fill out all questions before submitting.");
+                return;
+            }
+
+            if (guesses[i].guess === guesses[i].answer) {
+                setNumCorrect(numCorrect+1);
+            }
+        }
+        setSubmitted(true);
+    }
+
+    function handleRetake() {
+        setSubmitted(false);
+        setGuesses(initialGuesses);
+    }
+
+    const quizTemplate = (
+        <div>
+            <h1 className="title">Quiz</h1>
+            {questions}
+            <h2>All done! Ready to submit your test?</h2>
+            <div className="submit-btn" onClick={handleSubmit}>Submit test</div>
+            <div className="warning">{message}</div>
+        </div>
+    );
+
+    const results = [];
+    for (let i=0; i<props.cards.length; i++) {
+        const result = (
+            <div className="result-card">
+                <h5>{"Term: " + props.cards[i].name}</h5>
+                <div className="red">{(guesses[i].guess===guesses[i].answer) ? null : "✕"} </div>                
+                <div>{(guesses[i].guess===guesses[i].answer) ? null : guesses[i].guess}</div>
+
+                <div className="green">✓</div>
+                <div>{guesses[i].answer}</div>
+            </div>
+        );
+        results.push(result);
+    }
+
+    const resultsTemplate = (
+        <div>
+            <h1 className="title">Results</h1>
+            <p>{"Score: " + numCorrect + "/" + guesses.length}</p>
+            {results}
+            <div className="submit-btn" onClick={handleRetake}>Retake Quiz</div>
+        </div>
+    );
+    return (
+        <div>
+            {submitted ? resultsTemplate : quizTemplate}
+        </div>
+    )
+}
+
+export default Quiz;
